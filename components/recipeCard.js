f8bfa81b9bec4634523e677f00303915bc7be2a2
Koray Dundar <koraydundar_40@hotmail.com>
2023-02-09 13:10:05 +0200

    Using images

--- components/recipeCard.js
+++ components/recipeCard.js
@@ -1,13 +1,16 @@
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default function recipeCard({ recipe }) {
     const {title, cookingTime, featuredImage, ingredient, method, slug, thumbnail} = recipe.fields;
-
     return (
         <div className="card">
             <div className="featured">
                 {/* image thumb */}
-
+                <Image
+                    src={'https:' + thumbnail.fields.file.url} 
+                    width={thumbnail.fields.file.details.image.width}
+                    height={thumbnail.fields.file.details.image.height}
+                />
             </div>
             <div className="content">
                 <div className="info">
@@ -15,7 +18,7 @@ export default function recipeCard({ recipe }) {
                     <p>Takes approx { cookingTime } mins to make</p>
                 </div>
                 <div className="actions">
-                    <Link href={`/recipes/${slug}`} legacyBehavior>Cook this!</Link>
+                    <Link href={`/recipes/${slug}`} legacyBehavior>Cook this! </Link>
                 </div>
             </div>
         </div>
