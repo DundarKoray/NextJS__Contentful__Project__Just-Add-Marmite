3686197d2285864ed44400d653a3d30b9fdab11b
Koray Dundar <koraydundar_40@hotmail.com>
2023-02-09 13:15:03 +0200

    commenting

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import { createClient } from "contentful";
 import RecipeCard from "../components/recipeCard";
 
+//fetching data from Contentful
 export async function getStaticProps() {
 
+  //contentful credentials from .env.local file
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_KEY
   }); 
 
+  //getting recipe model type from contentful
   const res = await client.getEntries({ content_type: 'recipe' })
 
+  //exporting the data as props
   return {
     props: {
       recipes: res.items
@@ -19,7 +23,7 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
-  console.log(recipes);
+  // console.log(recipes);
 
   return (
     <div className="recipe-list">
