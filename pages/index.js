565e5167f225b9a18a1bf3dccb7e34f2d78f8065
Koray Dundar <koraydundar_40@hotmail.com>
2023-02-10 17:07:41 +0200

    generated paths

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export async function getStaticProps() {
 }
 
 export default function Recipes({ recipes }) {
-  // console.log(recipes);
+  console.log(recipes);
   let check = false;
 
   return (
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,7 +1,50 @@
-export default function RecipeDetails() {
+import { createClient } from "contentful";
+
+//contentful credentials from .env.local file
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY
+});
+
+// creating dynamic pages amount of repice items
+export const getStaticPaths = async () => {
+  //getting recipe model type from contentful
+  const res = await client.getEntries({ content_type: 'recipe' })
+  
+  const paths = res.items.map(item => {
+    return {
+      params: { slug: item.fields.slug }
+    }
+  })
+
+  return {
+    // because the names are the same we can just write path
+    // paths: paths,
+    paths,
+    fallback: false
+  }
+}
+
+//fetching data from Contentful
+export async function getStaticProps({ params }) {
+  
+  const { items } = await client.getEntries({ 
+    content_type: 'recipe',
+    // we dont want to fetch every recipe item but just one
+    'fields.slug': params.slug 
+  })
+
+  return {
+    props: { recipe: items[0] }
+  }
+
+}
+
+export default function RecipeDetails({ recipe }) {
+  console.log(recipe);
   return (
     <div>
-      Recipe Details
+      <h1>{recipe.fields.title}</h1>
     </div>
   )
 }
