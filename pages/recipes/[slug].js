6f9298cba924b9d085c1ebbbac19bfe015579da1
Koray Dundar <koraydundar_40@hotmail.com>
2023-02-13 17:20:55 +0200

    generated path and fetched data from rich text editor

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 //contentful credentials from .env.local file
 const client = createClient({
@@ -42,9 +44,57 @@ export async function getStaticProps({ params }) {
 
 export default function RecipeDetails({ recipe }) {
   console.log(recipe);
+  const { featuredImage, ingredient, method, thumbnail, title, cookingTime } = recipe.fields;
+
   return (
     <div>
-      <h1>{recipe.fields.title}</h1>
+      <div className="banner">
+        <Image 
+          src={'https:' + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        />
+        <h2>{ title }</h2>
+      </div>
+
+      <div className="info">
+        <p>It takes about {cookingTime} mins.</p>
+        <h3>Ingredients:</h3>
+        {ingredient.map((item, index)=>{
+          return <span key={index}>{ item }</span> ;
+        })}
+      </div>
+
+      <div className="method">
+        <h3>Method:</h3>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+      
+      <style jsx>{`
+        h2,h3 {
+          text-transform: uppercase;
+        }
+        .banner h2 {
+          margin: 0;
+          background: #fff;
+          display: inline-block;
+          padding: 20px;
+          position: relative;
+          top: -60px;
+          left: -10px;
+          transform: rotateZ(-1deg);
+          box-shadow: 1px 3px 5px rgba(0,0,0,0.1);
+        }
+        .info p {
+          margin: 0;
+        }
+        .info span::after {
+          content: ", ";
+        }
+        .info span:last-child::after {
+          content: ".";
+        }
+      `}</style>
     </div>
   )
 }
